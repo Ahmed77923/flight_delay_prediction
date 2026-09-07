b8b9a8c78e04325df61ec4e4aafa19746bb9697c
Ahmed77923 <Lyooa75@gmail.com>
2026-09-07 20:30:37 +0200

    Add prometheus

--- src/api/main.py
+++ src/api/main.py
@@ -5,7 +5,7 @@ from contextlib import asynccontextmanager
 
 import pandas as pd
 from fastapi import FastAPI, HTTPException
-
+from prometheus_fastapi_instrumentator import Instrumentator
 from src.api.model_loader import (
     get_state,
     load_pipeline,
@@ -110,7 +110,7 @@ app = FastAPI(
     version="1.0.0",
     lifespan=lifespan,
 )
-
+Instrumentator().instrument(app).expose(app)
 
 # ============================================================
 # ROOT
