2fc5d142921a497c90a107d4e061cb2bb6be9829
Ahmed77923 <Lyooa75@gmail.com>
2026-09-13 17:07:29 +0200

    Add same error in testing

--- tests/load/locustfile.py
+++ tests/load/locustfile.py
@@ -1,11 +1,8 @@
-
 from locust import HttpUser, task, between
+import random
 
 
 class FlightDelayUser(HttpUser):
-    """
-    Simulates a user interacting with the Flight Delay API.
-    """
 
     wait_time = between(1, 3)
 
@@ -13,11 +10,11 @@ class FlightDelayUser(HttpUser):
     def health_check(self):
         self.client.get(
             "/health",
-            name="/health"
+            name="/health",
         )
 
-    @task(5)
-    def predict(self):
+    @task(97)
+    def predict_valid(self):
         payload = {
             "FL_DATE": "2026-09-08T14:30:00",
             "CRS_DEP_TIME": 1430,
@@ -26,11 +23,30 @@ class FlightDelayUser(HttpUser):
             "DISTANCE": 760.0,
             "OP_UNIQUE_CARRIER": "AA",
             "ORIGIN": "JFK",
-            "DEST": "LAX"
+            "DEST": "LAX",
         }
 
         self.client.post(
             "/predict",
             json=payload,
-            name="/predict"
+            name="/predict-valid",
+        )
+
+    @task(30)
+    def predict_invalid(self):
+        payload = {
+            "FL_DATE": "WRONG-DATE",
+            "CRS_DEP_TIME": "INVALID",
+            "CRS_ARR_TIME": -999,
+            "CRS_ELAPSED_TIME": "wrong",
+            "DISTANCE": "wrong",
+            "OP_UNIQUE_CARRIER": "",
+            "ORIGIN": "INVALID",
+            "DEST": "INVALID",
+        }
+
+        self.client.post(
+            "/predict",
+            json=payload,
+            name="/predict-invalid",
         )
